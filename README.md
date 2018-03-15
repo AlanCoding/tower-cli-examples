6eee254f982c4b0a1d8f1f73cbb590eb724ea985
AlanCoding <arominge@redhat.com>
2018-03-15 09:05:05 -0400

    updates to looping script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tower-cli-examples
 Repository to share and maintain scripts that manage common data sets with tower-cli
+
+## bash
+
+Most of the examples are in this folder.
+These examples are written in bash
+
+### looping
+
+This tries to create 25 of most common resource types.
+
+```
+source bash/looping.sh
+```
+
+In dev environment, this script has been timed to take 20 minutes.
+This may grow as more resources are added.
--- bash/looping.sh
+++ bash/looping.sh
@@ -8,10 +8,21 @@ for i in `seq 1 25`; do
     tower-cli inventory create --name=inv$i --organization=Default --description="$cli_flag"
     tower-cli group create --inventory=inv$i --name=group$i --description="$cli_flag"
     tower-cli host create --name=group$i --inventory=inv$i --description="$cli_flag"
+    tower-cli inventory_script create --name=script$i --script='#!/usr/bin/env python\nimport json\nprint json.dumps({"ungrouped": ["foobar"]})' --organization=Default
+    tower-cli inventory_source create --name=source$i --source=custom --source-script=script$i --inventory=inv$i
     tower-cli project create --name=proj$i --organization=Default --scm-type git --scm-url https://github.com/AlanCoding/permission-testing-playbooks.git --description="$cli_flag" --wait
     tower-cli credential create --name=cred$i --inputs='{"password": "pass", "username": "pi"}' --organization=Default --credential-type="Machine" --description="$cli_flag"
+    # TODO: add credential type
     tower-cli job_template create --name=jt$i --inventory=inv$i --credential=cred$i --project=proj$i --playbook=helloworld.yml --description="$cli_flag"
+    # TODO: add schedules
+    # TODO: add labels
+    # TODO: add notification templates
+    # TODO: add workflow job template nodes
+
     tower-cli organization create --name=org$i --description="$cli_flag"
+    tower-cli user create --username=user$i --password password --email asdf@asdf.com
+    tower-cli team create --name=team$i --organization=org$i
+    # TODO: add applications
 
     tower-cli role grant --type use --inventory inv$i --user res_perm
     tower-cli role grant --type use --project proj$i --user res_perm
